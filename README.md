dc583134a64b9a78cbe64a3f9b720b1b2a284da2
rfmf <rfmf@protonmail.com>
2022-09-04 23:45:37 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Data Science Cookiecutter
+=========================
+
+Inspired by [DrivenData's data science cookiecutter](https://github.com/drivendata/cookiecutter-data-science).
+
+It is meant to be a simpler take on the aforementioned.
---
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+        name='src',
+        packages=find_packages(),
+        version='0.1.0',
+        description='{{ cookiecutter.description }}',
+        author='{{ cookiecutter.author_name }}',
+        license='{% if cookiecutter.license== 'MIT' %}MIT{% elif cookiecutter.license == 'BSD-3-Clause' %}BSD-3{% elif cookiecutter.license == 'Proprietary' %}Proprietary{% endif %}'
+)
